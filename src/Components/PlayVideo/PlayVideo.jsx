30a220ad2577bd445e36b9b139cb63b7be2e031d
Ethaniconic <e10.anthony345@gmail.com>
2025-06-27 14:31:20 +0530

    Finalised all changes and deployed successfully

--- src/Components/PlayVideo/PlayVideo.jsx
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -6,44 +6,80 @@ import dislike from "../../assets/dislike.png";
 import share from "../../assets/share.png";
 import save from "../../assets/save.png";
 import jack from "../../assets/jack.png";
-import user_profile from "../../assets/user_profile.jpg";
+// import user_profile from "../../assets/user_profile.jpg";
 import { value_converter } from "../../data";
 import moment from "moment";
+import { useParams } from "react-router-dom";
 
-const PlayVideo = ({ videoId }) => {
+const PlayVideo = () => {
+  const {videoId} = useParams();
 
   const [apiData, setApiData] = useState(null);
+  const [channelData, setChannelData] = useState(null);
+  const [commentData, setCommentData] = useState([]);
 
   const fetchVideoData = async () => {
     // Fetching Videos data
-    const videoDetail_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${import.meta.env.VITE_YOUTUBE_API}`;
-    await fetch(videoDetail_url).then(res => res.json()).then(data => setApiData(data.items[0]));
-  }
+    const videoDetail_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=${videoId}&key=${
+      import.meta.env.VITE_YOUTUBE_API
+    }`;
+    await fetch(videoDetail_url)
+      .then((res) => res.json())
+      .then((data) => setApiData(data.items[0]));
+  };
+
+  const fetchOtherData = async () => {
+    // Fetching channel data
+    const channelData_url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${
+      apiData.snippet.channelId
+    }&key=${import.meta.env.VITE_YOUTUBE_API}`;
+    await fetch(channelData_url)
+      .then((res) => res.json())
+      .then((data) => setChannelData(data.items[0]));
+
+    //Fetching comment data
+    const comment_url = `https://youtube.googleapis.com/youtube/v3/commentThreads?part=snippet%2Creplies&maxResults=50&videoId=${videoId}&key=${
+      import.meta.env.VITE_YOUTUBE_API
+    }`;
+
+    await fetch(comment_url)
+      .then((res) => res.json())
+      .then((data) => setCommentData(data.items));
+  };
 
   useEffect(() => {
     fetchVideoData();
-    console.log(apiData);
-  },[])
+  }, [videoId]);
+
+  useEffect(() => {
+    fetchOtherData();
+  }, [apiData]);
 
   return (
     <div className="play-video">
       {/* <video src={video1} controls autoPlay muted></video> */}
       <iframe
         src={`https://www.youtube.com/embed/${videoId}?autoplay=1`}
-        frameB order="0"
+        frameB
+        order="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         referrerpolicy="strict-origin-when-cross-origin"
         allowfullscreen
       ></iframe>
-      <h3>{apiData?apiData.snippet.title:"Title Here"}</h3>
+      <h3>{apiData ? apiData.snippet.title : "Title Here"}</h3>
       <div className="play-video-info">
-        <p>{value_converter(apiData?apiData.statistics.viewCount : "16k")} Views &bull; {apiData ? moment(apiData.snippet.publishedAt).fromNow() : "16k"}</p>
+        <p>
+          {value_converter(apiData ? apiData.statistics.viewCount : "16k")}{" "}
+          Views &bull;{" "}
+          {apiData ? moment(apiData.snippet.publishedAt).fromNow() : "16k"}
+        </p>
         <div>
           <span>
-            <img src={like} alt="" /> {apiData ? value_converter(apiData.statistics.likeCount) : 155}
+            <img src={like} alt="" />{" "}
+            {apiData ? value_converter(apiData.statistics.likeCount) : 155}
           </span>
           <span>
-            <img src={dislike} alt="" /> 2
+            <img src={dislike} alt="" />
           </span>
           <span>
             <img src={share} alt="" /> Share
@@ -55,107 +91,52 @@ const PlayVideo = ({ videoId }) => {
       </div>
       <hr />
       <div className="publisher">
-        <img src={jack} alt="" />
+        <img
+          src={channelData ? channelData.snippet.thumbnails.default.url : jack}
+          alt=""
+        />
         <div>
-          <p>GreatStack</p>
-          <span>1M Subscribers</span>
+          <p>{apiData ? apiData.snippet.channelTitle : ""}</p>
+          <span>
+            {channelData
+              ? value_converter(channelData.statistics.subscriberCount)
+              : "1M"}{" "}
+            Subscribers
+          </span>
         </div>
         <button>Subscribe</button>
       </div>
       <div className="vid-description">
-        <p>Channel that makes learning easy</p>
-        <p>Subscribe GreatStack to watch more tutorials on web dev</p>
+        <p>
+          {apiData
+            ? apiData.snippet.description.slice(0, 250)
+            : "Description here"}
+        </p>
         <hr />
-        <h4>130 Comments</h4>
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Jack Nicholson <span>1 day ago</span>
-            </h3>
-            <p>
-              One of the best tutorials I have watched so far after trying out
-              hundreds of boring tutorial and with a bad explaination. This
-              channel is a life saver
-            </p>
-            <div className="comment-action">
-              <img src={like} alt="" /> <span>244</span>
-              <img src={dislike} alt="" />
-            </div>
-          </div>
-        </div>
+        <h4>
+          {apiData ? value_converter(apiData.statistics.commentCount) : 102}{""}
+          Comments
+        </h4>
 
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Jack Nicholson <span>1 day ago</span>
-            </h3>
-            <p>
-              One of the best tutorials I have watched so far after trying out
-              hundreds of boring tutorial and with a bad explaination. This
-              channel is a life saver
-            </p>
-            <div className="comment-action">
-              <img src={like} alt="" /> <span>244</span>
-              <img src={dislike} alt="" />
+        {commentData.map((item, index) => {
+          return (
+            <div className="comment" key={index}>
+              <img src={item.snippet.topLevelComment.snippet.authorProfileImageUrl} alt="" />
+              <div>
+                <h3>
+                  {item.snippet.topLevelComment.snippet.authorDisplayName} <span>{moment(item.snippet.topLevelComment.snippet.publishedAt).fromNow()}</span>
+                </h3>
+                <p>
+                  {item.snippet.topLevelComment.snippet.textDisplay}
+                </p>
+                <div className="comment-action">
+                  <img src={like} alt="" /> <span>{value_converter(item.snippet.topLevelComment.snippet.likeCount)}</span>
+                  <img src={dislike} alt="" />
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Jack Nicholson <span>1 day ago</span>
-            </h3>
-            <p>
-              One of the best tutorials I have watched so far after trying out
-              hundreds of boring tutorial and with a bad explaination. This
-              channel is a life saver
-            </p>
-            <div className="comment-action">
-              <img src={like} alt="" /> <span>244</span>
-              <img src={dislike} alt="" />
-            </div>
-          </div>
-        </div>
-
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Jack Nicholson <span>1 day ago</span>
-            </h3>
-            <p>
-              One of the best tutorials I have watched so far after trying out
-              hundreds of boring tutorial and with a bad explaination. This
-              channel is a life saver
-            </p>
-            <div className="comment-action">
-              <img src={like} alt="" /> <span>244</span>
-              <img src={dislike} alt="" />
-            </div>
-          </div>
-        </div>
-
-        <div className="comment">
-          <img src={user_profile} alt="" />
-          <div>
-            <h3>
-              Jack Nicholson <span>1 day ago</span>
-            </h3>
-            <p>
-              One of the best tutorials I have watched so far after trying out
-              hundreds of boring tutorial and with a bad explaination. This
-              channel is a life saver
-            </p>
-            <div className="comment-action">
-              <img src={like} alt="" /> <span>244</span>
-              <img src={dislike} alt="" />
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
